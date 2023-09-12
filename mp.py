a280788994ff2481ab15a237d2f32f3adc34e1eb
Patrick Kage <patrick.r.kage@gmail.com>
2023-09-12 14:35:06 -0400

    run black over all source files

--- mp.py
+++ mp.py
@@ -42,43 +42,65 @@ def cleanup():
 
 
 def main(rank, world_size):
-    wandb.init(project='torch_test', entity='ai2es',
+    wandb.init(
+        project="torch_test",
+        entity="ai2es",
         name=f"{rank}: SimCLR Test",
         config={
-            'experiment': 'torch_test',
-        })
+            "experiment": "torch_test",
+        },
+    )
     print(f"Running basic DDP example on rank {rank} with world_size {world_size}.")
     setup(rank, world_size)
 
-    transform = transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ])
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
-    tr = torchvision.datasets.CIFAR10('../cifar-10/', train=True, download=True, transform=transform)
-    val = torchvision.datasets.CIFAR10('../cifar-10/', train=False, download=True, transform=transform)
+    tr = torchvision.datasets.CIFAR10(
+        "../cifar-10/", train=True, download=True, transform=transform
+    )
+    val = torchvision.datasets.CIFAR10(
+        "../cifar-10/", train=False, download=True, transform=transform
+    )
 
-    sampler_tr = DistributedSampler(tr,
-                             num_replicas=world_size,
-                             rank=rank,
-                             shuffle=True,  # May be True
-                             seed=42, 
-                             drop_last=True)
-    sampler_val = DistributedSampler(val,
-                             num_replicas=world_size,
-                             rank=rank,
-                             seed=42, 
-                             shuffle=False, 
-                             drop_last=True)
+    sampler_tr = DistributedSampler(
+        tr,
+        num_replicas=world_size,
+        rank=rank,
+        shuffle=True,  # May be True
+        seed=42,
+        drop_last=True,
+    )
+    sampler_val = DistributedSampler(
+        val, num_replicas=world_size, rank=rank, seed=42, shuffle=False, drop_last=True
+    )
 
     n_views, batch_size = 2, 512
 
-    train_loader = DataLoader(tr, batch_size=batch_size, shuffle=False, sampler=sampler_tr, pin_memory=True, num_workers=16, drop_last=True)
-    valid_loader = DataLoader(val, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=16)
+    train_loader = DataLoader(
+        tr,
+        batch_size=batch_size,
+        shuffle=False,
+        sampler=sampler_tr,
+        pin_memory=True,
+        num_workers=16,
+        drop_last=True,
+    )
+    valid_loader = DataLoader(
+        val, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=16
+    )
 
-    device = torch.device(f"cuda:{rank % torch.cuda.device_count()}") if torch.cuda.is_available() else torch.device("cpu")
+    device = (
+        torch.device(f"cuda:{rank % torch.cuda.device_count()}")
+        if torch.cuda.is_available()
+        else torch.device("cpu")
+    )
 
-    cnn_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', weights=None)
+    cnn_model = torch.hub.load("pytorch/vision:v0.10.0", "resnet50", weights=None)
     torch.cuda.set_device(rank % torch.cuda.device_count())
 
     # have to send the module to the correct device first
@@ -87,20 +109,32 @@ def main(rank, world_size):
     auto_wrap_policy = functools.partial(
         size_based_auto_wrap_policy, min_num_params=1_000_000
     )
-    model = FSDP(cnn_model, 
-            auto_wrap_policy=auto_wrap_policy,
-            mixed_precision=torch.distributed.fsdp.MixedPrecision(
-                param_dtype=torch.float32, 
-                reduce_dtype=torch.float32, 
-                buffer_dtype=torch.float32, 
-                cast_forward_inputs=True)
-            )
+    model = FSDP(
+        cnn_model,
+        auto_wrap_policy=auto_wrap_policy,
+        mixed_precision=torch.distributed.fsdp.MixedPrecision(
+            param_dtype=torch.float32,
+            reduce_dtype=torch.float32,
+            buffer_dtype=torch.float32,
+            cast_forward_inputs=True,
+        ),
+    )
 
     opt = torch.optim.AdamW(model.parameters(), lr=0.5)
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, 'min')
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, "min")
     # important to not use precision here because it is included in FSDP
-    args = {'epochs': 300, 'device': device, 'fp16_precision': False, 'disable_cuda': False, 'temperature': .1, 'n_views': n_views, 'batch_size': batch_size, 'log_every_n_steps': 100, 'arch': 'resnet50'}
+    args = {
+        "epochs": 300,
+        "device": device,
+        "fp16_precision": False,
+        "disable_cuda": False,
+        "temperature": 0.1,
+        "n_views": n_views,
+        "batch_size": batch_size,
+        "log_every_n_steps": 100,
+        "arch": "resnet50",
+    }
     args = Namespace(**args)
 
     model = SimCLR(model=model, optimizer=opt, scheduler=scheduler, args=args)
--- simclr.py
+++ simclr.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 from torch.cuda.amp import GradScaler, autocast
 from torch.utils.tensorboard import SummaryWriter
 from tqdm.rich import tqdm
+
 # from rich.progress import track
 from utils import save_config_file, accuracy, save_checkpoint
 
@@ -20,18 +21,23 @@ torch.manual_seed(0)
 
 
 class SimCLR(object):
-
     def __init__(self, *args, **kwargs):
-        self.args = kwargs['args']
-        self.model = kwargs['model'].to(self.args.device)
-        self.optimizer = kwargs['optimizer']
-        self.scheduler = kwargs['scheduler']
+        self.args = kwargs["args"]
+        self.model = kwargs["model"].to(self.args.device)
+        self.optimizer = kwargs["optimizer"]
+        self.scheduler = kwargs["scheduler"]
         self.writer = SummaryWriter()
-        logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
+        logging.basicConfig(
+            filename=os.path.join(self.writer.log_dir, "training.log"),
+            level=logging.DEBUG,
+        )
         self.criterion = torch.nn.CrossEntropyLoss().to(self.args.device)
 
     def info_nce_loss(self, features):
-        labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
+        labels = torch.cat(
+            [torch.arange(self.args.batch_size) for i in range(self.args.n_views)],
+            dim=0,
+        )
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
         labels = labels.to(self.args.device)
 
@@ -45,14 +51,18 @@ class SimCLR(object):
         # discard the main diagonal from both: labels and similarities matrix
         mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.args.device)
         labels = labels[~mask].view(labels.shape[0], -1)
-        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+        similarity_matrix = similarity_matrix[~mask].view(
+            similarity_matrix.shape[0], -1
+        )
         # assert similarity_matrix.shape == labels.shape
 
         # select and combine multiple positives
         positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
 
         # select only the negatives the negatives
-        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+        negatives = similarity_matrix[~labels.bool()].view(
+            similarity_matrix.shape[0], -1
+        )
 
         logits = torch.cat([positives, negatives], dim=1)
         labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)
@@ -75,14 +85,24 @@ class SimCLR(object):
             for images, _ in tqdm(train_loader):
                 images = images.to(self.args.device)
                 color_jitter = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
-                data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=images.size(-1), antialias=True),
-                                                    transforms.RandomHorizontalFlip(),
-                                                    transforms.RandomApply([color_jitter], p=0.8),
-                                                    transforms.RandomGrayscale(p=0.2),
-                                                    transforms.GaussianBlur(kernel_size=int(0.1 * images.size(-1)) - ((1 + int(0.1 * images.size(-1))) % 2))
-                                                    ])
+                data_transforms = transforms.Compose(
+                    [
+                        transforms.RandomResizedCrop(
+                            size=images.size(-1), antialias=True
+                        ),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomApply([color_jitter], p=0.8),
+                        transforms.RandomGrayscale(p=0.2),
+                        transforms.GaussianBlur(
+                            kernel_size=int(0.1 * images.size(-1))
+                            - ((1 + int(0.1 * images.size(-1))) % 2)
+                        ),
+                    ]
+                )
 
-                images = torch.cat([data_transforms(images) for i in range(self.args.n_views)], dim=0)
+                images = torch.cat(
+                    [data_transforms(images) for i in range(self.args.n_views)], dim=0
+                )
 
                 with autocast(enabled=self.args.fp16_precision):
                     features = self.model(images)
@@ -98,10 +118,14 @@ class SimCLR(object):
 
                 if n_iter % self.args.log_every_n_steps == 0:
                     top1, top5 = accuracy(logits, labels, topk=(1, 5))
-                    self.writer.add_scalar('loss', loss, global_step=n_iter)
-                    self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
-                    self.writer.add_scalar('acc/top5', top5[0], global_step=n_iter)
-                    self.writer.add_scalar('learning_rate', self.optimizer.defaults['lr'], global_step=n_iter)
+                    self.writer.add_scalar("loss", loss, global_step=n_iter)
+                    self.writer.add_scalar("acc/top1", top1[0], global_step=n_iter)
+                    self.writer.add_scalar("acc/top5", top5[0], global_step=n_iter)
+                    self.writer.add_scalar(
+                        "learning_rate",
+                        self.optimizer.defaults["lr"],
+                        global_step=n_iter,
+                    )
 
                 n_iter += 1
 
@@ -112,11 +136,17 @@ class SimCLR(object):
 
         logging.info("Training has finished.")
         # save model checkpoints
-        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.args.epochs)
-        save_checkpoint({
-            'epoch': self.args.epochs,
-            'arch': self.args.arch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
-        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
+        checkpoint_name = "checkpoint_{:04d}.pth.tar".format(self.args.epochs)
+        save_checkpoint(
+            {
+                "epoch": self.args.epochs,
+                "arch": self.args.arch,
+                "state_dict": self.model.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+            },
+            is_best=False,
+            filename=os.path.join(self.writer.log_dir, checkpoint_name),
+        )
+        logging.info(
+            f"Model checkpoint and metadata has been saved at {self.writer.log_dir}."
+        )
